3b134d04f91bb403cb205460f1b89436a6fe01be
tengxiaoyang <656877449@qq.com>
2021-03-07 15:39:08 +0800

    add or modify

---
+++ src/module_01.js
@@ -0,0 +1,4 @@
+export const name = 'pangpang'
+export function sayHello() {
+  console.log('hello')
+} //上面两个直接export有作用域
---
+++ src/module_02.js
@@ -0,0 +1,2 @@
+const otherName = 'xiaopang'
+export default otherName //export default使整个模块默认暴露
