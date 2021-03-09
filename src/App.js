813a0cc2c58c10f3bf21386a7d986fbd17400e2d
tengxiaoyang <656877449@qq.com>
2021-03-09 22:34:08 +0800

    add or modify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addGun, removeGun, addGunAsync } from './index.redux'
 
 class App extends React.Component {
   // constructor(props) {
@@ -6,20 +8,21 @@ class App extends React.Component {
   // }
   render() {
     // 通过属性获取store以及addGun、removeGun这两个操作函数，再通过store获取初始状态和dispatch方法：
-    const store = this.props.store
-    const num = store.getState()
-    const addGun = this.props.addGun
-    const removeGun = this.props.removeGun
-    const addGunAsync =this.props.addGunAsync
     return (
       <div>
-        <h1>现在有机枪{num}把</h1>
-        <button onClick={() => store.dispatch(addGun())}>申请武器</button>
-        <button onClick={() => store.dispatch(removeGun())}>上交武器</button>
-        <button onClick={() => store.dispatch(addGunAsync())}>拖两天再给</button>
+        <h1>现在有机枪{this.props.num}把</h1>
+        <button onClick={this.props.addGun}>申请武器</button>
+        <button onClick={this.props.removeGun}>上交武器</button>
+        <button onClick={this.props.addGunAsync}>拖两天再给</button>
       </div>
     )
   }
 }
 
+const mapStateToProps = (state) => {
+  return {num: state} //把状态放到属性里
+}
+const actionCreators = { addGun, removeGun, addGunAsync } //把方法传入
+
+App = connect(mapStateToProps, actionCreators)(App) //先执行connect，再把App当做参数传入，这就是一个装饰器
 export default App
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import App from './App';
-import { counter, addGun, removeGun, addGunAsync } from './index.redux'
+import { counter } from './index.redux'
 
 // 新建Store，并且以组件属性的形式传入组件里：
 const store = createStore(counter, compose(
@@ -13,21 +14,13 @@ const store = createStore(counter, compose(
 
 applyMiddleware(thunk)
 
-function render() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App 
-        store={store} 
-        addGun={addGun}
-        removeGun={removeGun}
-        addGunAsync={addGunAsync}
-      />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-render()
+ReactDOM.render(
+  (<Provider store={store}>
+    <App />
+  </Provider>),
+  document.getElementById('root')
+); //Provider组件在应用最外层，传入store，并且只需要用一次，Connect负责从外部获取组件需要的参数，Connect可以用装饰器的方式来写
 
 // 用subscribe订阅render函数，当状态变化时render会重新执行，重新渲染整个页面：
-store.subscribe(render)
+// store.subscribe(render)
 
