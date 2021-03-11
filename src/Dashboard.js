1b430ef1350da294ca9897ff60f6b71c3b8381f8
tengxiaoyang <656877449@qq.com>
2021-03-12 00:06:28 +0800

    add or modify

---
+++ src/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        Dashboard page
+        {/* <ul>
+          <li>
+            <Link to='/'>一营</Link>
+          </li>
+          <li>
+            <Link to='/erying'>二营</Link>
+          </li>
+          <li>
+            <Link to='/qibinglian'>骑兵连</Link>
+          </li>
+        </ul> */}
+      </div>
+    )
+  }
+}
+
+export default Dashboard
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { 
+  BrowserRouter, 
+  Route, 
+  Link, 
+  Redirect,
+  Switch
+} from 'react-router-dom'
 
 import App from './App';
 import { counter } from './index.redux'
+import Auth from './Auth'
+import Dashboard from './Dashboard'
 
 // 新建Store，并且以组件属性的形式传入组件里：
 const store = createStore(counter, compose(
@@ -23,30 +31,35 @@ function Qibinglian() {
   return <h2>骑兵连</h2>
 }
 
+class Test extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    console.log(this.props)
+    // this.props.history.push('/')
+    return <h2>测试组件 {this.props.match.params.location}</h2>
+  }
+}
+
+// 页面设计：
+// 登录
+//   如果没有登录信息，统一跳转到login
+// 页面 导航+显示+注销
+//   一营
+//   二营
+//   骑兵连
+// 全部用redux来管理，route+redux
 ReactDOM.render(
   (<Provider store={store}>
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to='/'>一营</Link>
-        </li>
-        <li>
-          <Link to='/erying'>二营</Link>
-        </li>
-        <li>
-          <Link to='/qibinglian'>骑兵连</Link>
-        </li>
-      </ul>
-      <div>
-        <Route path='/' exact component={App}></Route>
-        <Route path='/erying' component={Erying}></Route>
-        <Route path='/qibinglian' component={Qibinglian}></Route>
-      </div>
+      <Switch>
+        <Route path='/login' component={Auth}></Route>
+        <Route path='/dashboard' component={Dashboard}></Route>
+        <Redirect to='/dashboard'></Redirect>
+      </Switch> {/*只渲染命中的第一个Route*/}
     </BrowserRouter>
   </Provider>),
   document.getElementById('root')
 ); //Provider组件在应用最外层，传入store，并且只需要用一次，Connect负责从外部获取组件需要的参数，Connect可以用装饰器的方式来写
-
-// 用subscribe订阅render函数，当状态变化时render会重新执行，重新渲染整个页面：
-// store.subscribe(render)
 
