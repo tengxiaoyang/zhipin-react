f1aa7f8d60b56cce7fb50f162f2e139dfd2c35a4
tengxiaoyang <656877449@qq.com>
2021-03-14 17:50:57 +0800

    add or modify

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,3 @@
-// const { override, fixBabelImports, addDecoratorsLegacy } = require('customize-cra');
-
-// module.exports = override(
-//   fixBabelImports(
-//     'import', 
-//     {
-//       libraryName: 'antd-mobile',
-//       libraryDirectory: "es",
-//       style: 'css'
-//     } //antd按需加载
-//   ),
-//   addDecoratorsLegacy() //配置装饰器
-// );
-
 const { override, addLessLoader, fixBabelImports, addDecoratorsLegacy } = require('customize-cra');
 module.exports = override(
   addLessLoader({
