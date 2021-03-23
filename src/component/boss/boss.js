6807c1b815b76e82bc90f140a5bd6bf4080b8620
tengxiaoyang <656877449@qq.com>
2021-03-23 23:45:05 +0800

    add or modify

--- src/component/boss/boss.js
+++ src/component/boss/boss.js
@@ -1,30 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react'
 import axios from 'axios'
-import { Card, WhiteSpace, WingBlank } from "antd-mobile";
- 
-class Boss extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
-  componentDidMount() {
-    axios.get('/user/list?type=genius')
-      .then(res => {
-        if (res.data.code==0) {
-          this.setState({data: res.data.data})
-        }
-      })
-  }
-  render() { 
-    console.log(this.state)
-    return (
-      <div>
-        Boss
-      </div>
-    );
-  }
+import {connect} from 'react-redux'
+import {Card, WhiteSpace,WingBlank} from 'antd-mobile'
+import {getUserList} from '../../redux/chatuser.redux'
+import UserCard from '../usercard/usercard'
+
+@connect(
+	state=>state.chatuser,
+	{getUserList}
+)
+class Boss extends React.Component{
+	componentDidMount() {
+		this.props.getUserList('genius')
+	}
+	render(){
+		return <UserCard userlist={this.props.userlist}></UserCard>
+	}
+
 }
- 
-export default Boss;
+export default Boss
